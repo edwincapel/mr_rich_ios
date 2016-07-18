541f0cd0954a6341b084a16ed0a52387ffbd4814
Edwin Capel <edwincapel@edwins-mbp.attlocal.net>
2016-07-17 23:29:05 -0500

    Mr.Rich version 1

---
+++ MrRichIOS/Assets/Scripts/Score.cs
@@ -0,0 +1,179 @@
+﻿using Facebook.Unity;
+using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public AudioClip coinSound;
+	public Text scoreText;
+	public Text highScoreText;
+	public Text currentScore;
+	public GameObject EnemyPrefab;
+	public GameObject money;
+	public int moneyValue;
+	public static int score;
+	public int highScore;
+	public float currentScale = 0.5f;
+	public string fileNameShare;
+	string ScoreText;
+
+
+	public void TweetLove(){
+
+		int unicode = 10084;
+		char character = (char) unicode;
+		string text = character.ToString();
+
+		string TWITTER_ADDRESS = "http://twitter.com/intent/tweet";
+		string TWEET_LANGUAGE = "en";
+
+		Application.OpenURL(TWITTER_ADDRESS +
+			"?text=" + WWW.EscapeURL("I " + text + " Mr Rich !" + "\nDownload Mr Rich from the appstore now !  \n\nhttp://mrrichthega.me/\nhttp://gph.is/1MIkOaI")) ;
+
+	}
+
+	public void ShareLoveFb()
+	{
+
+		int unicode = 10084;
+		char character = (char) unicode;
+		string text = character.ToString();
+		string fullText = "I " + text + " Mr Rich !";
+
+		FB.FeedShare (
+			string.Empty,
+			new Uri("http://mrrichthega.me/"),
+			fullText,
+			"Download this game and beat my highscore !",
+			"Help Mr.Rich gain his wealth now",	new Uri("http://i.imgur.com/NiJpVsU.jpg"),
+			string.Empty,
+			ShareCallback
+		);
+	}
+
+	public void Reset(){
+
+		score = 0;
+	}
+
+	void Awake ()
+	{
+		if (FB.IsInitialized) {
+			FB.ActivateApp();
+		} else {
+			//Handle FB.Init
+			FB.Init( () => {
+				FB.ActivateApp();
+			});
+		}
+	}
+
+	void OnApplicationPause (bool pauseStatus)
+	{
+		// Check the pauseStatus to see if we are in the foreground
+		// or background
+		if (!pauseStatus) {
+			//app resume
+			if (FB.IsInitialized) {
+				FB.ActivateApp();
+			} else {
+				//Handle FB.Init
+				FB.Init( () => {
+					FB.ActivateApp();
+				});
+			}
+		}
+	}
+
+
+	public void Share()
+	{
+
+		highScore = PlayerPrefs.GetInt("HighScore");
+		int currentScore2 = score;
+		if (currentScore2 > highScore) {
+
+			ScoreText = "I just made a New high score of " + PlayerPrefs.GetInt ("HighScore") + " !";
+
+		} 
+		else if (currentScore2 == highScore) {
+			ScoreText = "So Close ! Almost beat my own highscore of " + PlayerPrefs.GetInt ("HighScore") + " !";
+
+		}else if (currentScore2 > 0) {
+			ScoreText = "I just scored " + currentScore2 + " and my high score is " + PlayerPrefs.GetInt ("HighScore") + " !";
+
+		} else {
+			ScoreText = "My high score is " + PlayerPrefs.GetInt ("HighScore") + " ! Try and beat me !";
+		}
+
+		FB.FeedShare (
+			string.Empty,
+			new Uri("http://mrrichthega.me/"),
+			ScoreText,
+			"Download this game and beat my highscore !",
+			"Help Mr.Rich gain his wealth now",	new Uri("http://i.imgur.com/NiJpVsU.jpg"),
+			string.Empty,
+			ShareCallback
+		);
+	}
+
+	void ShareCallback (IResult result)
+	{
+		if (result.Cancelled) {
+			Debug.Log ("Share Cancelled");
+		} else if (!string.IsNullOrEmpty (result.Error)) {
+			Debug.Log ("Error on share!");
+		} else if (!string.IsNullOrEmpty (result.RawResult)) {
+			Debug.Log ("Success on share");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		EnemyPrefab.GetComponent<Rigidbody2D>().gravityScale = currentScale;
+		money.GetComponent<Rigidbody2D>().gravityScale = currentScale;
+		UpdateScore ();
+		if(PlayerPrefs.HasKey("HighScore")){
+			highScore = PlayerPrefs.GetInt("HighScore");
+			highScoreText.text = "" + highScore;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		if (col.gameObject.name == "moneymo" || col.gameObject.name == "moneymo(Clone)") {
+			AudioSource.PlayClipAtPoint (coinSound, transform.position);
+			score += moneyValue;
+			UpdateScore ();
+		}
+
+	}
+
+	void UpdateScore(){
+		scoreText.text = "" + score;
+		currentScore.text = "" + score;
+		if (score > highScore) {
+			highScore = score;
+			highScoreText.text = "" + highScore;
+			PlayerPrefs.SetInt ("HighScore", highScore);
+		}
+		if (score >= 10 && score<=20) {
+			currentScale = 0.7f;
+			EnemyPrefab.GetComponent<Rigidbody2D>().gravityScale = currentScale;
+			money.GetComponent<Rigidbody2D>().gravityScale = currentScale;
+		}
+		else if (score >= 21 && score<=30) {
+			currentScale = 0.9f;
+			EnemyPrefab.GetComponent<Rigidbody2D>().gravityScale = currentScale;
+			money.GetComponent<Rigidbody2D>().gravityScale = currentScale;
+		}
+		else if (score >= 31 && score<=1000) {
+			currentScale = 1.1f;
+			EnemyPrefab.GetComponent<Rigidbody2D>().gravityScale = currentScale;
+			money.GetComponent<Rigidbody2D>().gravityScale = currentScale;
+		}
+
+	}
+}
---
+++ MrRichIOS/Assets/Scripts/destroy.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using GoogleMobileAds;
+using GoogleMobileAds.Api;
+using System;
+
+public class destroy : MonoBehaviour {
+
+	private BannerView bannerView;
+
+	void Start(){
+	}
+		
+
+	private void RequestBanner()
+	{
+		#if UNITY_EDITOR
+		string adUnitId = "unused";
+		#elif UNITY_ANDROID
+		string adUnitId = "INSERT_ANDROID_BANNER_AD_UNIT_ID_HERE";
+		#elif UNITY_IPHONE
+		string adUnitId = "ca-app-pub-8590188300139671/2420793141";
+		#else
+		string adUnitId = "unexpected_platform";
+		#endif
+
+		// Create a 320x50 banner at the top of the screen.
+		bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Bottom);
+		// Register for ad events.
+		bannerView.OnAdLoaded += HandleAdLoaded;
+		bannerView.OnAdFailedToLoad += HandleAdFailedToLoad;
+		bannerView.OnAdLoaded += HandleAdOpened;
+		bannerView.OnAdClosed += HandleAdClosed;
+		bannerView.OnAdLeavingApplication += HandleAdLeftApplication;
+		// Load a banner ad.
+		bannerView.LoadAd(createAdRequest());
+		}
+
+		private AdRequest createAdRequest()
+		{
+		return new AdRequest.Builder()
+		.AddTestDevice(AdRequest.TestDeviceSimulator)
+		.AddTestDevice("0123456789ABCDEF0123456789ABCDEF")
+		.AddKeyword("game")
+		.SetGender(Gender.Male)
+		.SetBirthday(new DateTime(1985, 1, 1))
+		.TagForChildDirectedTreatment(false)
+		.AddExtra("color_bg", "9B30FF")
+		.Build();
+		}
+
+		public void HandleAdLoaded(object sender, EventArgs args)
+		{
+		print("HandleAdLoaded event received.");
+		}
+
+		public void HandleAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+		{
+		print("HandleFailedToReceiveAd event received with message: " + args.Message);
+		}
+
+		public void HandleAdOpened(object sender, EventArgs args)
+		{
+		print("HandleAdOpened event received");
+		}
+
+		void HandleAdClosing(object sender, EventArgs args)
+		{
+		print("HandleAdClosing event received");
+		}
+
+		public void HandleAdClosed(object sender, EventArgs args)
+		{
+		print("HandleAdClosed event received");
+		}
+
+		public void HandleAdLeftApplication(object sender, EventArgs args)
+		{
+		print("HandleAdLeftApplication event received");
+		}
+
+
+		#region Interstitial callback handlers
+
+		public void HandleInterstitialLoaded(object sender, EventArgs args)
+		{
+		print("HandleInterstitialLoaded event received.");
+		}
+
+		public void HandleInterstitialFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+		{
+		print("HandleInterstitialFailedToLoad event received with message: " + args.Message);
+		}
+
+		public void HandleInterstitialOpened(object sender, EventArgs args)
+		{
+		print("HandleInterstitialOpened event received");
+		}
+
+		void HandleInterstitialClosing(object sender, EventArgs args)
+		{
+		print("HandleInterstitialClosing event received");
+		}
+
+		public void HandleInterstitialClosed(object sender, EventArgs args)
+		{
+		print("HandleInterstitialClosed event received");
+		}
+
+		public void HandleInterstitialLeftApplication(object sender, EventArgs args)
+		{
+		print("HandleInterstitialLeftApplication event received");
+		}
+
+		#endregion
+
+		}
